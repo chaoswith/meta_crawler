a0a15c32e69928484aef0ed07779eece197211f8
chaoswith <chaoswith@gmail.com>
2022-03-08 15:38:20 +0900

    lint 수정

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -1,17 +1,17 @@
-import * as mongoDB from "mongodb";
+import * as mongoDB from 'mongodb';
 
 const MONGODB_URL = process.env.MONGODB_URL;
 const MONGODB_NAME = process.env.MONGODB_NAME;
 const MONGODB_COLLECTION_NAME = process.env.MONGODB_COLLECTION_NAME;
 
-export const collections: { metadatas?: mongoDB.Collection } = {}
+export const collections: { metadatas?: mongoDB.Collection } = {};
 
 export function connectToDatabase() {
-   const client: mongoDB.MongoClient = new mongoDB.MongoClient(MONGODB_URL!);
-   client.connect();
-   const db: mongoDB.Db = client.db(MONGODB_NAME!);
-   const metadatasCollection: mongoDB.Collection = db.collection(MONGODB_COLLECTION_NAME!);
-   collections.metadatas = metadatasCollection;
-   console.log(`Successfully connected to database: ${db.databaseName} and collection: ${metadatasCollection.collectionName}`);
+  const client: mongoDB.MongoClient = new mongoDB.MongoClient(MONGODB_URL!);
+  client.connect();
+  const db: mongoDB.Db = client.db(MONGODB_NAME!);
+  const metadatasCollection: mongoDB.Collection = db.collection(MONGODB_COLLECTION_NAME!);
+  collections.metadatas = metadatasCollection;
+  console.log(`Successfully connected to database: ${db.databaseName} and collection: ${metadatasCollection.collectionName}`);
 }
 export default collections;
